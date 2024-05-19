c4f6580922af34c06e15ebbb4cbf509d0ec3502f
Anton_algo <167683642+AlgoTest3@users.noreply.github.com>
2024-05-19 15:58:08 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from pygame import *
-init()
-
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
         super().__init__()
@@ -26,9 +24,10 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < win_width - 80:
+        if keys[K_s] and self.rect.y > win_width - 80:
             self.rect.y += self.speed
-
+        
+init()
 back = (200, 255, 255)
 win_width = 600
 win_height = 500
@@ -36,14 +35,23 @@ window = display.set_mode((win_width, win_height))
 window.fill(back)
 
 game = True
-finish = False 
+finish = False
 clock = time.Clock()
 FPS = 60
 
+racket1 = Player('racket.png', 30, 200, 50, 150, 4)
+racket2 = Player('racket.png', 520, 200, 50, 150, 4)
+ball = GameSprite('tenis_ball.png', 200, 200, 50, 50, 5)
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
     display.update()
     clock.tick(FPS)
